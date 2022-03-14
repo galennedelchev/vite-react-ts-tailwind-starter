e4aa36bf948990b64cc7a934152cabf9dd58335e
Galen Nedelchev <galen.nedelchev@rascasone.com>
2022-03-14 14:44:46 +0100

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import Diagram from "./pages/Diagram";
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import IndexPage from './pages'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path='/'>
-          <Diagram/>
+        <Route exact path="/">
+          <IndexPage />
         </Route>
       </Switch>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default App;
+export default App
